13ff3ee2e50f52e3cf6c381a62ecdb1404c7ca5b
Victor Sosa <vsosa01127@gmail.com>
2022-04-19 21:58:05 -0400

    Add project files.

---
+++ Data/DataUtility.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTracker.Data
+{
+    public class DataUtility
+    {
+        public static string GetConnectionString(IConfiguration configuration)
+        {
+            var localHost = configuration.GetConnectionString("DefaultConnection");
+            var externalHostUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+            return string.IsNullOrEmpty(externalHostUrl) ? localHost : BuildConnectionString(externalHostUrl);
+        }
+
+        private static string BuildConnectionString(string externalHostUrl)
+        {
+            var hostUri = new Uri(externalHostUrl);
+            var userInfo = hostUri.UserInfo.Split(':');
+
+            var output = new NpgsqlConnectionStringBuilder()
+            {
+                Host = hostUri.Host,
+                Port = hostUri.Port,
+                Username = userInfo[0],
+                Password = userInfo[1],
+                Database = hostUri.LocalPath.TrimStart('/'),
+                SslMode = SslMode.Prefer,
+                TrustServerCertificate = true
+            };
+
+            return output.ToString();
+        }
+    }
+}
---
+++ Models/AppUser.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTracker.Models
+{
+    public class AppUser : IdentityUser
+    {
+        [Required]
+        [Display(Name = "First name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last name")]
+        public string LastName { get; set; }
+
+        [NotMapped]
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
+
+        public byte[] ImageData { get; set; }
+        public string ImageType { get; set; }
+           
+        [NotMapped]
+        public IFormFile ImageFile { get; set; }
+
+    }
+}
